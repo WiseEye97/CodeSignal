c2c205711d4b6898406655b22a14fdfedc532a35
WiseEye <36448457+WiseEyeKOSO@users.noreply.github.com>
2018-11-18 22:52:10 +0100

    Add files via upload

--- meanscores.fs
+++ meanscores.fs
@@ -1,17 +1,37 @@
-(*
-       Given scores, an array of integers representing all your test and assignment grades, 
-       your task is to return an array of integers where output[i] represents the mean of all your scores up to index i.
-*)
-let meanScores scores =
-        scores
-        |> Array.fold (fun s e ->
-                let ar,ar2,i = s
-                ((if i > 0 then Array.get ar (i - 1) else 0) + e) |> Array.set ar i
-                Array.set ar2 i (ar.[i] / (i + 1))
-                ar , ar2 , i + 1 
-        ) (Array.zeroCreate scores.Length , Array.zeroCreate scores.Length , 0)
-        |> (function 
-            | _,r,_ -> r
-                )      
+open System.Text
 
-meanScores <| [|100; 20; 50; 70; 45|]  
+(*
+    The set [1, 2, 3, ... , n] contains a total of n! unique permutations. 
+    List all the permutations for an integer n in lexicographical order and return the kth permutation in the sequence as a string. 
+    To build this string, concatenate decimal representations of permutation elements from left to right without any delimiters.
+*)
+
+let permutationSequence n k =
+
+    let rec toFactorial = function
+        | 0 , li -> li
+        | x , li -> 
+                    let d = (li |> List.length) + 1
+                    let r = x%d
+                    toFactorial ((x/d),r::li)
+    let rec generateString li (res : StringBuilder) (strLi : string list)  = 
+        match li with
+        | [] -> res
+        | h::t -> 
+                  strLi 
+                  |> List.splitAt h
+                  |> (function 
+                    | l1 , l2 -> 
+                                res.Append(List.head l2) |> ignore
+                                generateString t res (l1 @ (List.tail l2))
+                  )
+    let fNum = if k = 1 then [0] else  toFactorial(k - 1,[])
+
+    [for i in 1..n -> 
+                     i 
+                     |> string   
+                                ]
+    |> generateString fNum (new StringBuilder())
+    |> (fun sb -> sb.ToString()) 
+
+permutationSequence 3 2
