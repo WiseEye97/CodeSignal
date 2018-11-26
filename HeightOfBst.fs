376e61f3e465cef03cc8500476cbc664ec48b342
WiseEye <36448457+WiseEyeKOSO@users.noreply.github.com>
2018-11-26 10:40:12 +0100

    Add files via upload

---
+++ HeightOfBst.fs
@@ -0,0 +1,31 @@
+﻿
+
+type Node = {
+    Left : Option<Node>
+    Right : Option<Node>
+    V : int
+}
+
+let heightOfBST values =
+    let rec addToBst node i num = 
+        match node with
+        | Some n ->
+                    let v = n.V
+                    match num - v with
+                    | x when x > 0 -> let right,r = (addToBst n.Right (i + 1) num)
+                                      Some {n with Right = right},r
+                    | _ -> let left,r = (addToBst n.Left (i + 1) num) 
+                           Some {n with Left = left},r
+        | _ -> (Some {Left = None ; Right = None; V = num}),i
+    match values with
+    | [||] -> 0
+    | [|_|] -> 1
+    | x -> x |> Array.fold (fun s e -> 
+                            let t,i = addToBst (s |> fst) 1 e
+                            t , (s |> snd |> max i)
+                           ) (None,0)
+             |> snd
+   
+    
+
+heightOfBST [|34; 96; -73; 10; -98; 25|]
